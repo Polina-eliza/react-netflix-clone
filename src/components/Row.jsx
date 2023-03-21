7bc5b720226aedb844536b41da6b4407f72ceddf
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-03-21 09:25:00 +0200

    created headings for movies row's

---
+++ src/components/Row.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Row = ({title, fetchURL}) => {
+  return (
+    <>
+    <h2 className='text-white font-bold md:text-x; p-4'>{title}</h2>
+    <div className='relative flex items-center group'></div>
+
+    </>
+  )
+}
+
+export default Row
